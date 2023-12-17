81641a602ee5f7f1473f33b4e88d5c8bce3bf8f0
Kardelen Kubat <45011431+kardelenkubat@users.noreply.github.com>
2023-12-17 21:20:44 +0300

    Add files via upload

---
+++ chatApp/Hubs/MyHub.cs
@@ -0,0 +1,48 @@
+﻿using chatApp.Interface;
+using Microsoft.AspNetCore.SignalR;
+
+// MyHub sınıfı, SignalR'ın Hub sınıfından türetilir ve IMessageClient arayüzünü kullanır.
+public class MyHub : Hub<IMessageClient>
+{
+    // Tüm bağlı istemcilerin Connection ID'lerini tutan statik bir liste.
+    static List<string> clients = new List<string>();
+
+    // Bir istemci bağlandığında çalışacak olan metot.
+    public override async Task OnConnectedAsync()
+    {
+        // Yeni bağlanan istemcinin Connection ID'sini listeye ekler.
+        clients.Add(Context.ConnectionId);
+
+        // Tüm bağlı istemcilere güncel istemci listesini ve yeni bağlanan istemciyi bildirir.
+        await Clients.All.Clients(clients);
+        await Clients.All.UserJoined(Context.ConnectionId);
+    }
+
+    // Tüm istemcilere mesaj göndermek için kullanılan metot.
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.Send(message);
+    }
+
+    // Bir istemci bağlantıyı kestiğinde çalışacak olan metot.
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        // Bağlantısı kesilen istemcinin ID'sini listeden çıkarır.
+        clients.Remove(Context.ConnectionId);
+        // Tüm bağlı istemcilere güncel istemci listesini ve ayrılan istemciyi bildirir.
+        await Clients.All.Clients(clients);
+        await Clients.All.UserLeaved(Context.ConnectionId);
+    }
+
+    // Çizim yapılmasını sağlayan metot. Çizim verilerini tüm bağlı istemcilere iletir.
+    public async Task SendDraw(string type, string startPos, string endPos, string color, int lineWidth)
+    {
+        await Clients.All.ReceiveDraw(type, startPos, endPos, color, lineWidth);
+    }
+
+    // Tüm bağlı istemcilerde çizim tahtasını temizleyen metot.
+    public async Task ClearBoard()
+    {
+        await Clients.All.ClearBoard();
+    }
+}
---
+++ chatApp/Interface/IMessageClient.cs
@@ -0,0 +1,25 @@
+﻿namespace chatApp.Interface
+{
+    // IMessageClient, SignalR ile istemciye gönderilecek mesajların arayüzünü tanımlar.
+    public interface IMessageClient
+    {
+        // Tüm bağlı istemcilerin listesini istemcilere gönderir.
+        Task Clients(List<string> clients);
+
+        // Bir istemcinin bağlandığını tüm istemcilere bildirir.
+        Task UserJoined(string connectionId);
+
+        // Bir istemcinin ayrıldığını tüm istemcilere bildirir.
+        Task UserLeaved(string connectionId);
+
+        // Tüm istemcilere genel bir mesaj gönderir.
+        Task Send(string message);
+
+        // Tüm istemcilerde çizim tahtasını temizlemek için kullanılır.
+        Task ClearBoard();
+
+        // Çizim verilerini almak ve bu verileri istemcilere göndermek için kullanılır.
+        // Parametreler arasında çizim tipi, başlangıç ve bitiş pozisyonları, renk ve çizgi kalınlığı bulunur.
+        Task ReceiveDraw(string type, string startPos, string endPos, string color, int lineWidth);
+    }
+}
